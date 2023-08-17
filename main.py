32d9d9151b19c2f1c1ee7426a97ad880df31fe82
pheineke <p_heineke22@cs.uni-kl.de>
2023-08-18 00:46:11 +0200

    Paar gute Änderungen

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 from functools import reduce
 
+def remove_duplicates(input_str):
+    words = input_str.split()
+    unique_words = reduce(lambda x, y: x if y in x else x + [y], [[], ] + words)
+    return ' '.join(unique_words)
 
 def get_table_content(url):
     response = requests.get(url)
@@ -12,6 +16,7 @@ def get_table_content(url):
     if table:
         rows = table.find_all('tr')
         table_content = "\n".join([row.get_text(strip=True) for row in rows])
+        table_content = remove_duplicates(table_content)
         table_content = table_content.replace("Quotierungszeitraum:", "\nQuotierungszeitraum:\n")
         table_content = table_content.replace("zugewiesene", "\nzugewiesene")
         table_content = table_content.replace("VerbrauchLimit", "\nVerbrauchLimit")
@@ -23,10 +28,7 @@ def get_table_content(url):
         table_content = "Keine Tabelle gefunden."
     return table_content
  
-def remove_duplicates(input_str):
-    words = input_str.split()
-    unique_words = reduce(lambda x, y: x if y in x else x + [y], [[], ] + words)
-    return ' '.join(unique_words)
+
 
 # Tkinter GUI
 root = tk.Tk()
@@ -37,7 +39,7 @@ text_widget.pack(fill=tk.BOTH, expand=True)
 
 url = "https://quota.wohnheim.uni-kl.de/"
 table_content = get_table_content(url)
-table_content = remove_duplicates(table_content)
+#table_content = remove_duplicates(table_content)
 text_widget.insert(tk.END, table_content)
 
 
